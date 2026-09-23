448b38fd5655dcd33b73598f526b4bd9a3194468
shashank-simha <shashanksimha183@gmail.com>
2026-09-23 23:35:00 +0200

    app: integrate LED sensor driver

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,35 +1,35 @@
-#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/sensor.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
-#if defined(CONFIG_APP_LED_SUBSYSTEM) && CONFIG_APP_LED_SUBSYSTEM
 #define SLEEP_TIME_MS CONFIG_APP_HEARTBEAT_PERIOD_MS
+#define LED_SENSOR_NODE DT_ALIAS(led_sensor)
 
-#define LED_NODE DT_ALIAS(app_led)
-
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
-#endif
+static const struct device *const led_sensor = DEVICE_DT_GET(LED_SENSOR_NODE);
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
 int main(void)
 {
-#if defined(CONFIG_APP_LED_SUBSYSTEM) && CONFIG_APP_LED_SUBSYSTEM
-    bool led_state = true;
+    struct sensor_value value;
 
-    if (!gpio_is_ready_dt(&led)) return 0;
+    if (!device_is_ready(led_sensor)) {
+        LOG_ERR("LED sensor is not ready");
+        return 0;
+    }
 
-    if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) return 0;
+    while (true) {
+        if (sensor_sample_fetch(led_sensor) < 0) {
+            LOG_ERR("LED sensor sample fetch failed");
+            return 0;
+        }
 
-    while (1) {
-        if (gpio_pin_toggle_dt(&led) < 0) return 0;
+        if (sensor_channel_get(led_sensor, SENSOR_CHAN_ALL, &value) < 0) {
+            LOG_ERR("LED sensor channel get failed");
+            return 0;
+        }
 
-        led_state = !led_state;
-#if defined(CONFIG_APP_LED_DEBUG) && CONFIG_APP_LED_DEBUG
-        LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
-#endif
+        LOG_INF("LED sensor value: %d", value.val1);
         k_msleep(SLEEP_TIME_MS);
     }
-#endif
-    return 0;
 }
